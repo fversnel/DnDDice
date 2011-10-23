e24280e190ddfd1afe290c2ff46aed0aeb1f8085
Frank Versnel <f.versnel@gmail.com>
2011-10-23 15:42:01 +0200

    Minor optimizations.

--- src/Roll.scala
+++ src/Roll.scala
@@ -7,7 +7,7 @@ class RollResult (val rolls: List[Int], val modifier: Modifier) {
 	def dieCount: Int = rolls.size
 	def total: Int = rolls.sum + modifier.value
 
-	override def toString:String = {
+	override def toString: String = {
 		val rollsString = rolls.map(_.toString).reduce(_ + " " + _)
 		val modifierString = if (modifier.nonNeutral) " (" + modifier + ")" else ""
 		val totalString = if (dieCount > 1 || modifier.nonNeutral) " [Total: " + total + "]" else ""
@@ -25,15 +25,14 @@ class Roll private(val dieCount: Int, val die: Die, val modifier: Modifier) {
 	override def toString = dieCount.toString + die.toString + modifier.toString
 }
 object Roll extends RollParser {
-	private val neutralModifier = Modifier(0)
 	private val oneThrow = 1
 
 	def apply(input: String): Option[Roll] = {
 		val parsedRoll = parse(input)
 		if (parsedRoll.successful) Some(parsedRoll.get) else None
 	}
-	def apply(die: Die) = new Roll(oneThrow, die, neutralModifier)
-	def apply(dieCount: Int, die: Die) = new Roll(dieCount, die, neutralModifier)
+	def apply(die: Die) = new Roll(oneThrow, die, Modifier.Neutral)
+	def apply(dieCount: Int, die: Die) = new Roll(dieCount, die, Modifier.Neutral)
 	def apply(die: Die, modifier: Modifier) = new Roll(oneThrow, die, modifier)
 	def apply(dieCount: Int, die: Die, modifier: Modifier) = new Roll(dieCount, die, modifier)
 }
@@ -43,6 +42,7 @@ class Modifier private(val value: Int) {
 	override def toString = if (value >= 0) "+" + value else value.toString
 }
 object Modifier {
+	val Neutral = new Modifier(0)
 	def apply(value: Int) = new Modifier(value)
 }
 
@@ -50,7 +50,6 @@ class Die private(val sides: Int, val numberGenerator: Random) {
 	private val SideOffset = 1
 
 	def roll = numberGenerator.nextInt(sides) + SideOffset
-
 	override def toString = "d" + sides
 }
 object Die {
