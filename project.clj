dbbfc98c42ecf3c00c89162e425cddefa22e3d9d
Frank Versnel <f.versnel@gmail.com>
2013-05-25 09:50:38 +0200

    Updated version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dnddice "2.0.0"
+(defproject dnddice "2.2.1"
   :description "Clojure library that parses Dungeons & Dragons dice rolls (e.g. '1d20' etc.) and 
 rolls them."
   :url "https://github.com/fversnel/DnDDice"
