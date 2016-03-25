cb107c4163f6292b1c1920ca3e3899bbced519cd
Frank Versnel <f.versnel@gmail.com>
2016-03-25 14:10:23 +0100

    WIP on supporting dice arithmetic.

--- src/org/fversnel/dnddice/parser.clj
+++ src/org/fversnel/dnddice/parser.clj
@@ -4,44 +4,38 @@
 
 (def roll-parser
   (insta/parser
-    "<roll> = (post-fix-roll | pre-fix-roll)
-     <post-fix-roll> = dice post-fix-modifier? drop-roll?
-     <pre-fix-roll> = pre-fix-modifier? dice drop-roll?
-     <dice> = die-count? sides
+    "<dice-expression> = (dice | value) (operator dice-expression)* drop-roll?
 
+     dice = die-count? sides
      die-count = integer-above-zero
      sides = <('d'|'D')> (integer-above-zero | '%')
+
+     operator = '+' | '-' | '/' | 'x'
+     value = integer
      drop-roll = <'-'> ('L' | 'H')
-     pre-fix-modifier = integer operator
-     post-fix-modifier = operator integer
-     <operator> = '+' | '-' | '/' | 'x'
 
      integer-above-zero = #'[1-9][0-9]*'
      integer = #'[0-9]+'"))
 
-(def roll-transform-options
-  (letfn [(create-modifier [operator value]
-            [:modifier {:operator (case operator
-                                    "+" '+
-                                    "-" '-
-                                    "x" '*
-                                    "/" '/)
-                        :value value}])]
-    {:integer-above-zero edn/read-string
-     :integer edn/read-string
-     :post-fix-modifier create-modifier
-     :pre-fix-modifier #(create-modifier %2 %1)
-     :drop-roll #(case %
-                   "L" [:drop :lowest]
-                   "H" [:drop :highest])}))
+; [[:dice {:sides x :die-count y}]
+;  operator
+;  [:dice {:sides x :die-count y}]
+;  operator
+;  [:value value]
+;  [:drop type]
 
-(defn- to-roll [parse-tree] 
-   (if (insta/failure? parse-tree)
-     (throw (IllegalArgumentException. "Failed to parse roll."))
-     (reduce (fn [roll part]
-               (assoc roll (first part) (second part))) {} parse-tree)))
+(def roll-transform-options
+  {:dice (fn [& parts]
+           [:dice (apply hash-map (flatten parts))])
+   :integer-above-zero edn/read-string
+   :integer edn/read-string
+   :operator #(case % "+" '+
+                      "-" '-
+                      "x" '*
+                      "/" '/)
+   :drop-roll #(case % "L" [:drop :lowest]
+                       "H" [:drop :highest])})
 
 (defn parse [^String input-str]
   (->> (roll-parser input-str)
-    (insta/transform roll-transform-options)
-    (to-roll)))
+    (insta/transform roll-transform-options)))
